895656316a903e04ebee46027e6053914aa8c27c
kamil-b-dev <benedykowiak@gmail.com>
2026-01-26 18:42:48 +0100

    Utworzenie: Entity gem i item dto dla gem service dla dodawania kamienia do przedmiotu controller dla nowego service plik z testową bazą danych

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO items (id, name, category, slot, req_level) VALUES (1, 'aaa', 'bb', 'cc', 40);
+INSERT INTO item_stats (item_id, stat_name, stat_value) VALUES (1, 'MOC', 15);
+INSERT INTO item_stats (item_id, stat_name, stat_value) VALUES (1, 'WIEDZA', 10);
+INSERT INTO gems (id, name, bonus_stat, bonus_value, item_id) VALUES (101, 'aa', 'WIEDZA', 5, 1);
