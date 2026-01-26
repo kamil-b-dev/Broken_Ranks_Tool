253108f595ea84e4eaeefd8508076ff90717c9c8
lobozo13 <benedykowiak@gmail.com>
2026-01-26 16:19:57 +0100

    Initialize repository

---
+++ src/main/java/pl/brokenranks/tool/broken_ranks_tool/BrokenRanksToolApplication.java
@@ -0,0 +1,13 @@
+package pl.brokenranks.tool.broken_ranks_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokenRanksToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokenRanksToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/brokenranks/tool/broken_ranks_tool/BrokenRanksToolApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.brokenranks.tool.broken_ranks_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokenRanksToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
